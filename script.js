a0b8d608df9043a2ac43b40b8741f33c5454d29d
MahmoodElsaayed <Mahmood.M.Elsaayed@gmail.com>
2024-03-17 20:47:01 -0700

    Initialize project setup
    - create project files and directories
    - import project assets
    - add basic HTML, CSS and JS content to their respective files
    - link project files

---
+++ script.js
@@ -0,0 +1,9 @@
+const myLibrary = [];
+
+function Book(title, author, status, rating, cover) {
+    this.title = title;
+    this.author = author;
+    this.status = status;
+    this.rating = rating;
+    this.cover = cover;
+}
